ad31300e1b763c375f64b05b89b941dd5e67e83c
FreeBz <freebz@hananet.net>
2018-10-02 10:22:39 +0900

    chapter 1 스칼라 준비하기

---
+++ ch01/Ex_1_1.scala
@@ -0,0 +1,9 @@
+// 코드 1-1 헬로, 스칼라!
+
+package test
+
+object Ex_1_1 {
+  def main(args: Array[String]): Unit = {
+    println("헬로, 스칼라!")
+  }
+}
---
+++ ch01/Ex_1_2.java
@@ -0,0 +1,9 @@
+// 코드 1-2 헬로, 자바!
+
+package test;
+
+public class Ex_1_2 {
+    public static void main(String[] args) {
+	System.out.println("헬로, 자바!");
+    }
+}
